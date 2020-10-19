a6a2d23662908d5429d43db8ecb79b38fcce734e
Andrew Eames <ajerxs@gmail.com>
2020-10-19 20:45:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board = [" "," "," "," "," "," "," "," "," "]
-def display_board
+def display_board(board)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
@@ -9,4 +9,4 @@ def display_board
   puts "   |   |   "
 end
 
-display_board
+display_board(board)
